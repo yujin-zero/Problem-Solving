30d1daf1f2a77c68cb4bbdaaa24a9a83b76652f0
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2023-07-10 18:50:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1448 ms, Memory: 77108 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+num_list = []
+for _ in range(n):
+    num_list.append(int(sys.stdin.readline()))
+
+num_list.sort()
+
+for i in num_list:
+    print(i)
