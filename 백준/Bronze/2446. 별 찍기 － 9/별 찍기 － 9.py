78f00181cb52db617f4b39caf09d07bca29cd59f
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2023-08-01 02:40:44 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+for i in range(n, 0, -1):
+    for j in range(n-i):
+        print(' ', end='')
+    for j in range(2*i-1):
+        print('*', end='')
+    print()
+for i in range(2, n+1):
+    for j in range(n-i):
+        print(' ', end='')
+    for j in range(2*i-1):
+        print('*', end='')
+    print()
