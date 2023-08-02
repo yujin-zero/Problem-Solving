80ae38c42c113f85694463ce2737c6e7c981f10c
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2023-08-03 03:38:27 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,35 +1,41 @@
 import sys
 from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
-people = [[] for _ in range(n+1)]
 
-for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
-    if b not in people[a]:
-        people[a].append(b)
-        people[b].append(a)
+def bfs(v):
+    global cnt, mn, ans
+    q = deque()
+    q.append(v)
+    visited = [0]*101  # 몇번만에 해당 노드로 도착하는지 계산
+    visited[v] = 1  # 자기 노드는 처음부터 도착하므로 1
+    while q:
+        t = q.popleft()
+        for i in graph[t]:
+            if not visited[i]:
+                q.append(i)
+                # 이전에 노드까지 가는데 걸리는 시간 + 현재 노드까지 한칸더 이동
+                visited[i] = visited[t] + 1
+    cnt = sum(visited)  # v가 모든 노드를 가는데 걸리는 횟수
+    if mn > cnt:  # 만약 mn 보다 cnt 가 작으면
+        mn = cnt  # mn은 cnt가 되고
+        ans = v  # 답은 시작한 노드
 
-kevinNum = n**2
-kevinPerson = 0
 
-for i in range(1, n+1):
-    # 케빈 베이컨 구하기
-    queue = deque([i])
-    visit = deque([i])
-    kevin = {}
-    kevin[i] = 0
-    while queue:
-        temp = queue.popleft()
-        for p in people[temp]:
-            if p not in visit:
-                visit.append(p)
-                queue.append(p)
-                kevin[p] = kevin[temp] + 1
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 
-    s = sum(kevin.values())
-    if kevinNum > s:
-        kevinNum = s
-        kevinPerson = i
+lst = []
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)  # graph 인접리스트만들기
+    graph[b].append(a)
+    lst.append(a)  # 어떤 번호가 있는지 알기위해 lst에 집어넣기
+    lst.append(b)
 
-print(kevinPerson)
+mn = 1e9
+cnt = 0
+ans = 0
+for j in set(lst):  # lst에 있는 값으로 bfs진행
+    bfs(j)
+
+print(ans)
