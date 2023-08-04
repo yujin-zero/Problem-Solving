454c332d796234c49aa6d65baf92a3fbab5bfd2b
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2023-08-04 11:40:07 +0900

    [Silver I] Title: IOIOI, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,28 +4,15 @@ n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 s = sys.stdin.readline().rstrip()
 
+# I와 O로 이루어진 패턴 생성
+pattern = 'I' + 'OI'*n
 count = 0
-for i in range(0, m-2*n):
-    if s[i] == 'I':
-        temp = 1
-        check = 0
-        for j in range(i+1, 2*n + i+1):
-            if temp == 1:
-                if s[j] == 'O':
-                    temp = 0
-                else:
-                    check = 1
-                    break
-            else:
-                if s[j] == 'I':
-                    temp = 1
-                else:
-                    check = 1
-                    break
-    else:
-        check = 1
-    if check == 0:
-        count += 1
+i = 0
 
+while i < m - len(pattern) + 1:
+    # 패턴의 일치 여부를 확인
+    if s[i:i + len(pattern)] == pattern:
+        count += 1
+    i += 1
 
 print(count)
