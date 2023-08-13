fef607a51c820e8c01910d7d19df61939806007b
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2023-08-13 12:25:33 +0900

    [Gold V] Title: LCS, Time: 212 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+
+a = sys.stdin.readline().rstrip()
+b = sys.stdin.readline().rstrip()
+
+dp = [0] * len(b)
+for i in range(len(a)):
+    temp = 0
+    for j in range(len(b)):
+        if temp < dp[j]:
+            temp = dp[j]
+        elif a[i] == b[j]:
+            dp[j] = temp+1
+
+print(max(dp))
