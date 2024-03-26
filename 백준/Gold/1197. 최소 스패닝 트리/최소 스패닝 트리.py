0e19192b951eeea3168d4ee1dd99032e5c77b8c5
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2024-03-26 19:22:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 292 ms, Memory: 49216 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def find(x):
+    if parent[x] != x:
+        return find(parent[x])
+    return x
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, sys.stdin.readline().split())
+tree = []
+answer = 0
+parent = [i for i in range(v+1)]
+visit = set()
+
+for _ in range(e):
+    a, b, c = map(int, sys.stdin.readline().split())
+    tree.append((a, b, c))
+tree.sort(key=lambda x: x[2], reverse=True)
+
+while tree:
+    node1, node2, weight = tree.pop()
+    if find(node1) != find(node2):  # 써클 생성되지 않음
+        answer += weight
+        union(node1, node2)
+
+print(answer)
