de0a358052e4f3d234f26ee5bb552ed90a0850b6
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2024-06-24 23:36:23 +0900

    [Silver II] Title: 동전 게임, Time: 368 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/9079. 동전 게임/동전 게임.py
@@ -0,0 +1,56 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+reverse = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+for _ in range(t) :
+    coin = []
+    for i in range(3) :
+        line = sys.stdin.readline().split()
+        for l in line :
+            if l == 'H' :
+                coin.append(False)
+            else :
+                coin.append(True)
+    queue = deque()
+    queue.append((coin,0))
+    visit = []
+    visit.append(coin)
+
+    p = 0
+
+    while queue :
+        tmp,cnt = queue.popleft()
+
+        a = 0
+        for i in range(8) :
+            if tmp[i]!=tmp[i+1] :
+                a = 1
+        if a==0 :
+            print(cnt)
+            p = 1
+            break
+        
+
+        for re in reverse :
+            x = [True for _ in range(9)]
+            for i in range(9) :
+                x[i] = tmp[i]
+            for r in re :
+                x[r] = not tmp[r]
+            a = 0
+            for v in visit :
+                aa = 0
+                for j in range(9) :
+                    if v[j] != x[j] :
+                        aa = 1
+                if aa == 0 :
+                    a = 1
+                    break
+            if a == 0 :
+                queue.append((x,cnt+1))
+                visit.append(x)
+
+    if p==0 :
+        print(-1)
