e465616620bb3229efc63f7f912defbe1d09e12d
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2024-07-28 02:40:13 +0900

    [Gold III] Title: 웜홀, Time: 1120 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,30 +1,33 @@
-from sys import stdin
-input = stdin.readline
+import sys
 
-def bf():
-      for i in range(n):
-            for j in range(len(edges)):
-                  cur, next, cost = edges[j]
-                  if dist[next] > dist[cur] + cost:
-                        dist[next] = dist[cur] + cost
-                        if i == n - 1:
-                              return True
-      return False
-                        
-TC = int(input())
+def bellman_ford() :
+    for i in range(n) :
+        for j in range(len(edge)) :
+            cur, next, cost = edge[j]
+            if dist[next] > dist[cur] + cost :
+                dist[next] = dist[cur] + cost
+                if i == n - 1:
+                    return True
+    
+    return False
+    
+tc = int(sys.stdin.readline())
 
-for _ in range(TC):
-      n, m, w = map(int, input().split())
-      edges = []
-      dist = [1e9] * (n + 1)
-      for i in range(m + w):
-            s, e, t = map(int, input().split())
-            if i >= m:
-                  t = -t
-            else:
-                  edges.append((e, s, t))
-            edges.append((s, e, t))
-      if bf():
-            print("YES")
-      else:
-            print("NO")
+for _ in range(tc) :
+    n, m, w = map(int,sys.stdin.readline().split())
+    edge = []
+    dist = [1e9] * (n + 1)
+
+    for _ in range(m) :
+        s, e, t = map(int,sys.stdin.readline().split())
+        edge.append((s, e, t))
+        edge.append((e, s, t))
+    
+    for _ in range(w) :
+        s, e, t = map(int,sys.stdin.readline().split())
+        edge.append((s, e, -t))
+
+    if bellman_ford() :
+        print("YES")
+    else :
+        print("NO")
