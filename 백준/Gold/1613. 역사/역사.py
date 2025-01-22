7cedb936c043fb0368e6429f0252fc64366eb0fe
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2025-01-22 12:12:48 +0900

    [Gold III] Title: 역사, Time: 3324 ms, Memory: 32804 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.py
+++ 백준/Gold/1613. 역사/역사.py
@@ -1,26 +1,27 @@
 import sys
 
 n, k = map(int,sys.stdin.readline().split())
-dp = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+dp = [[False for _ in range(n+1)] for _ in range(n+1)]
 for i in range(1,n+1) :
-    dp[i][i] = 0
+    dp[i][i] = True
 for _ in range(k) :
     a, b = map(int,sys.stdin.readline().split())
-    dp[a][b] = 1
+    dp[a][b] = True
 
 for k in range(1,n+1) :
     for i in range(1,n+1) :
-        if dp[i][k] == float('inf') :
+        if dp[i][k] == False :
             continue
         for j in range(1,n+1) :
-            dp[i][j] = min(dp[i][k] + dp[k][j],dp[i][j])
+            if dp[k][j] :
+                dp[i][j] = True
 
 s = int(sys.stdin.readline())
 for _ in range(s) :
     a, b = map(int,sys.stdin.readline().split())
-    if dp[a][b] != float('inf') :
+    if dp[a][b] :
         print(-1)
-    elif dp[b][a] != float('inf') :
+    elif dp[b][a] :
         print(1)
     else :
         print(0)
