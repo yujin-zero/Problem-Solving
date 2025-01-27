04dfd9a6d8302a9d1b1c89aafdf2f69c209c81c2
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2025-01-27 20:14:33 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 2296 ms, Memory: 66624 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -5,9 +5,9 @@ def axxb(a,b) :
         return 1
     elif b % 2 == 0 :
         tmp = axxb(a,b/2)
-        return tmp * tmp
+        return (tmp * tmp) % mod
     tmp = axxb(a,b-1)
-    return tmp * a
+    return (tmp * a) % mod
 
 N = int(sys.stdin.readline())
 hot = list(map(int,sys.stdin.readline().split()))
@@ -15,9 +15,9 @@ hot.sort()
 answer = 0
 mod = 1000000007
 
-for i in range(N) :
+for i in range((N+1)//2) :
     min_cnt = axxb(2,N-i-1)
     max_cnt = axxb(2,i)
-    answer += (hot[i] * (max_cnt - min_cnt) % mod)
+    answer += hot[i] * (max_cnt - min_cnt) + hot[N-i-1] * (min_cnt - max_cnt)
 
 print(answer % mod)
