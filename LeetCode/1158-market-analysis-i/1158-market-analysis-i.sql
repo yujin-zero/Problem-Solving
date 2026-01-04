f35bd1a5359587d721425aacc13010efadcdb956
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2026-01-04 16:18:25 +0900

    Time: 2388 ms (5.02%), Space: 0B (100%) - LeetHub

--- LeetCode/1158-market-analysis-i/1158-market-analysis-i.sql
+++ LeetCode/1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,8 +1,5 @@
-with cte as (select buyer_id, count(*) as cnt
-from Orders
-where year(order_date) = 2019
-group by buyer_id)
-select user_id as buyer_id, join_date, ifnull(cnt, 0) as orders_in_2019
+select user_id as buyer_id, join_date, count(order_id) as orders_in_2019
 from Users u
-left join cte c
-on u.user_id = c.buyer_id
+left join Orders o
+on u.user_id = o.buyer_id and year(order_date) = 2019
+group by user_id
