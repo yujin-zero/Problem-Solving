730283dd17c997e643e3d8d3c1b0ee8b92877529
Yujin So <83626076+yujin-zero@users.noreply.github.com>
2026-03-24 19:03:23 +0900

    Time: 530 ms (43.62%), Space: 0B (100%) - LeetHub

--- LeetCode/3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ LeetCode/3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -3,8 +3,9 @@ from borrowing_records
 where return_date is null
 group by book_id)
 
-select *
+select lb.book_id, title, author, genre, publication_year, cnt as current_borrowers
 from library_books lb
 join cte c
 on lb.book_id = c.book_id
 where total_copies = cnt
+order by current_borrowers desc, title
